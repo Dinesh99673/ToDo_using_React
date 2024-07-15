9e859562ab6a0f8f84e8726994364b6615794881
Dinesh <dc7821836954@gmail.com>
2024-07-15 08:43:56 +0530

    second commit

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -12,7 +12,7 @@ function ToDoItem(props) {
                     <input type="checkbox" onChange={()=>(props.checkBox(props.id))}/>
                     <p className={props.flag?'line-through ml-2 mt-0':'ml-2 mt-0'}>{props.todo}</p>
                 </div>
-                <div className='flex gap-4 mr-[30%] mt-1'>
+                <div className='flex md:mr-[30%] mt-1'>
                     <button className='size-6 hover:bg-orange-500 p-0.5 rounded-md ' onClick={()=>(props.onEdit(props.id))} >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path></svg>
                     </button>
